cb913e37d9a603bcad78730f5b08365a68a7d638
Silas Rosenkranz <silas.rosenkranz@student.uni-siegen.de>
2020-04-18 18:55:47 +0200

    REMOVED: unused stuff

--- src/assets/theme.ts
+++ src/assets/theme.ts
@@ -1,5 +1,3 @@
-import { lighten as _lighten, darken as _darken } from 'polished';
-
 const preset = {
   colors: {
     primary: '#fff',
@@ -23,11 +21,5 @@ export type Color = 'primary' | 'accent1' | 'accent2';
 
 export const fontFamily = (f: FontFamily) => ({ theme: t }: ThemeProps): string => t.fonts[f];
 export const color = (c: Color) => ({ theme: t }: ThemeProps): string => t.colors[c];
-
-type Lighten = (v: number, c: Color) => ({ theme: t }: ThemeProps) => string;
-export const lighten: Lighten = (v, c) => ({ theme: t }): string => _lighten(v, t.colors[c]);
-
-type Darken = (v: number, c: Color) => ({ theme: t }: ThemeProps) => string;
-export const darken: Darken = (v, c) => ({ theme: t }): string => _darken(v, t.colors[c]);
 
 export default theme;
