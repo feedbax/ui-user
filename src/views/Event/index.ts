7cc8ad2d6c2ee1c4f44c6ecbb2f1b9950b25a637
Silas Rosenkranz <silas.rosenkranz@student.uni-siegen.de>
2020-05-19 11:33:24 +0200

    REFACTOR Event view styles

--- src/views/Event/index.ts
+++ src/views/Event/index.ts
@@ -1,22 +1,4 @@
-import { css } from 'styled-components';
-import { color } from 'assets/styles/theme';
-
 import Event from './Event';
 
 export const component = Event;
-
-export const styles = {
-  content: css`
-    padding: 0;
-    height: 100%;
-    max-width: 540px;
-    background-color: ${color('accent1')};
-
-    @media (min-width: 540px) {
-      max-height: 800px;
-      border-radius: 8px;
-      overflow: hidden;
-      box-shadow: 0 0 20px 0px rgba(0, 0, 0, 0.5);
-    }
-  `,
-};
+export { styles } from './styles';
---
+++ src/views/Event/styles.ts
@@ -0,0 +1,18 @@
+import { css } from 'styled-components';
+import { color } from 'assets/styles/theme';
+
+export const styles = {
+  content: css`
+    padding: 0;
+    height: 100%;
+    max-width: 540px;
+    background-color: ${color('accent1')};
+
+    @media (min-width: 540px) {
+      max-height: 800px;
+      border-radius: 8px;
+      overflow: hidden;
+      box-shadow: 0 0 20px 0px rgba(0, 0, 0, 0.5);
+    }
+  `,
+};
