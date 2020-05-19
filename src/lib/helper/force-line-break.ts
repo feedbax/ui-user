8b4d28707a3f12b0ed0b0de2498edc22ecef95af
Silas Rosenkranz <silas.rosenkranz@student.uni-siegen.de>
2020-05-19 11:19:59 +0200

    REFACTOR function readability

--- src/lib/helper/force-line-break.ts
+++ src/lib/helper/force-line-break.ts
@@ -2,8 +2,9 @@ export const forceLineBreak = <T extends HTMLElement>(el: T | null): void => {
   if (el) {
     const lineHeightString = window.getComputedStyle(el).lineHeight;
     const lineHeight = parseInt(lineHeightString, 10);
+    const isOnlyOneLine = el.clientHeight <= lineHeight + 1;
 
-    if (el.clientHeight <= lineHeight + 1) {
+    if (isOnlyOneLine) {
       // eslint-disable-next-line no-param-reassign
       el.style.maxWidth = `${0.8 * el.clientWidth}px`;
     }
